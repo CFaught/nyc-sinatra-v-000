bf02c775e3c8fb96fbe6920b7b621f018ca2fd7a
Caleb Faught <faughtcj@gmail.com>
2016-08-15 01:13:16 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,8 +1,17 @@
 class FiguresController < ApplicationController
-  
+
   get '/figures/:id/edit' do
     @figure = Figure.find_by(id: params[:id])
     erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    @figure.update(params[:figure])
+    @figure.titles << Title.create(name: params[:title][:name]) if !params[:title][:name].empty?
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
+    @figure.save
+    redirect "/figures/#{ @figure.id }"
   end
 
   get '/figures/new' do
@@ -12,6 +21,8 @@ class FiguresController < ApplicationController
 
   get '/figures/:id' do
     @figure = Figure.find_by(id: params[:id])
+    @titles = @figure.titles
+    @landmarks = @figure.landmarks
     erb :'/figures/show'
   end
 
@@ -20,7 +31,6 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    # binding.pry
     figure = Figure.create(name: params[:figure][:name])
 
     if !!params[:figure][:title_ids]
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
-  
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    redirect "/landmarks/#{ @landmark.id }"
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{ landmark.id }"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
 end
